4e84b0deab1cb00e271b8fcd8ddedc70a35521d9
Adnan Khayyat <adnan-car@windowslive.com>
2019-06-02 02:11:26 -0600

    Final

--- public/script.js
+++ public/script.js
@@ -3,10 +3,10 @@ var app = new Vue({
     data : {
         inquiry: '',
         loading: true,
-        ISBN: '',
+        ISBN: [],
+        thumbnails: [],
         myBooks: [
         ],
-        favs: [],
     },
     // title, author, publish date, cover image
 
@@ -22,14 +22,24 @@ var app = new Vue({
                 console.log(data);
                 this.loading = false;
                 for(var i = 0; i < data.docs.length; i++){
-                    // this.ISBN = data.docs[i].isbn[0];
-                    // const response2 = await axios.get('https://openlibrary.org/api/books?bibkeys=ISBN:' + this.ISBN + '&jscmd=details&format=json')
-                    // const data2 = response2.data;
-                    // console.log(data2);
 
-                    this.myBooks.push({title : data.docs[i].title_suggest, author : data.docs[i].author_name, publishDate : data.docs[i].first_publish_year, index : i});
-                 
+
+                    this.myBooks.push({title : data.docs[i].title_suggest, author : data.docs[i].author_name, publishDate : data.docs[i].first_publish_year, isbn: data.docs[i].isbn, thumbnail : '', isFave : false});
+                    if(typeof this.myBooks[i].isbn !== 'undefined'){
+                    this.ISBN = this.myBooks[i].isbn[0];
+                    console.log(this.ISBN);
+                    const response2 = await axios.get('https://openlibrary.org/api/books?bibkeys=ISBN:' + this.ISBN + '&jscmd=details&format=json')             
+                    const url = response2.data['ISBN:'+this.ISBN].thumbnail_url;
+                    console.log(url);
+                    if(typeof url !== 'undefined'){
+                        this.myBooks[i].thumbnail = url;
+                    }
+          
+                    }
+
+                  
                 }
+                
                 
                 return true;
             }
@@ -42,12 +52,15 @@ var app = new Vue({
      
         },
         FaveItem(item){
-            this.favs.push(myBooks[item]);
+            var index = this.myBooks.indexOf(item);
+            this.myBooks[index].isFave = true;
+
+        },
+        deleteItem(item){
+            var index = this.myBooks.indexOf(item);
+            this.myBooks[index].isFave = false;
         }
 
-    },
-    computed: {
-   
     },
 
 
